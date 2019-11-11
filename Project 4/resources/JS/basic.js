6c16279bfa7ce5e4e08f8e3b2f098bcdb45aca51
CosmicFishi <1851050025dung@ou.edu.vn>
2019-11-11 11:12:29 +0700

    changes

---
+++ Project 4/resources/JS/basic.js
@@ -0,0 +1,10 @@
+$(document).ready(function() {
+    var a = 0;
+    $("#btn1").click(function() {
+        a += 1;
+        $("p").text(a);
+    });
+    $("#btn2").click (function() {
+        $("p").text(a=0);
+    });
+});
